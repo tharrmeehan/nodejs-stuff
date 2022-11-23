9a73bf36b589de72dc76da6f68443e9d8cd6e5de
Tharrmeehan Krishnathasan <tharrmeehan@hotmail.com>
2022-11-23 17:36:51 +0100

    switch to express.js

--- app.js
+++ app.js
@@ -1,6 +1,25 @@
-const http = require("http");
-const routes = require("./routes");
+const express = require("express");
+const bodyParser = require("body-parser");
 
-const server = http.createServer(routes);
+const app = express();
 
-server.listen(3000);
+app.use(bodyParser.urlencoded());
+
+app.use("/add-product", (req, res, next) => {
+  console.log("in another middleware!");
+  res.send(
+    '<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</form>'
+  );
+});
+
+app.use("/product", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
+});
+
+app.use("/", (req, res, next) => {
+  console.log("in another middleware!");
+  res.send("<h1>Hello from Express!</h1>");
+});
+
+app.listen(3000);
