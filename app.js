ec008f1c9b83532b6e4420318be5f42ad0753611
Tharrmeehan Krishnathasan <tharrmeehan@hotmail.com>
2022-11-23 18:42:03 +0100

    changed app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
-app.use(bodyParser.urlencoded());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use("/add-product", (req, res, next) => {
   console.log("in another middleware!");
